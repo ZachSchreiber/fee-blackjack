a10aa7f914dc64eec79818edc02055b65035f1a3
Zachary Schreiber <zachgives@gmail.com>
2016-11-07 14:26:17 -0500

    spring cleaning

--- blackjack.js
+++ blackjack.js
@@ -1,13 +1,13 @@
-
 function runGame() {
-
+    //function to pick a random number and assign it to a card in the deck.
     var display = document.getElementById('cards');
-    cards = ['A', '2','3', '4','5',"6",'7','7','8', '9', "10", 'J', 'Q', 'K']
+    var cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
+    var cardValue = 0;
 
     function hit() {
+        checkResult(false, true);
         var card = Math.round(Math.random() * cards.length);
         display.innerHTML = cards[card];
-        checkResult('no', true);
     }
 
     /**
@@ -18,47 +18,64 @@ function runGame() {
      * @return {void}
      */
     function checkResult(standing, hitting) {
-        cards = display.innerHTML.split(' ');
+        hand = display.innerHTML.split(' ');
+        //  var cards = card or cards currently listed on the display.
 
-        var cardValue = 0;
 
-        cards.forEach(function (card, i) {
-            if (Number(card)) {
-                cardValue = cardValue + card;
+        hand.forEach(function(card, i) {
+
+            if (card[i] === 'J') {
+                cardValue += 10;
+            } else if (card[i] === 'Q') {
+                cardValue += 10;
+            } else if (card[i] === 'K') {
+                cardValue += 10;
+            } else if (card[i] === 'A') {
+                cardValue += 11;
+            } else if (Number(card)) {
+                cardValue = cardValue + Number(card);
             }
 
-            if (card === 'J' || card == 'Q' || card === 'J')
-                cardValue = cardValue + 10;
-
-            if (cards[i] = 'A') { cardValue = cardValue += 11; }
         });
+        console.log('cardvalue is: ' + cardValue);
+        console.log('card is: ' + card);
 
-        if (cardValue < 15 && standing) {
-            alert('Dealer wins.');
+          if (cardValue < 15 && standing) {
+              alert('Dealer wins.');
+          }
+        else if (cardValue < 18 && standing) {
+              alert('Push!');
+          }
+        else if (cardValue > 18 && hitting) {
+              alert('You win!');
+          }
+        else if (cardValue === 21) {
+              alert('You win!');
         }
-        if (cardValue < 18 && standing) {
-            alert('Push!');
-        }
-        if (cardValue > 18 & hitting || cardValue === 21) {
-            alert('You win!');
-        }
-        if (cardValue > 21) {
-            alert('You Bust.');
-        }
+        else if (cardValue > 21) {
+              alert('You Bust.');
+          }
 
-    display.innerHTML = '';
-    card = Math.round(Math.random() * cards.length);
-    display.innerHTML = cards[card];
+        display.innerHTML = ' ';
+        card = Math.round(Math.random() * cards.length);
+        display.innerHTML = cards[card];
+
     }
-
+    //   If stand button is clicked, trigger stand function
     document.getElementById('stand').addEventListener('click', function() {
-        checkREsult(true);
+        checkResult(true, false);
     });
 
-    document.getElementById('hit').addEventListener('click',function(){checkResult(null, true);});
+    //If hit button is clicked, trigger hit function
+    document.getElementById('hit').addEventListener('click', function() {
+        checkResult(false, true);
 
+    });
+
+    // Start the game with a hand of two cards
     card = Math.round(Math.random() * cards.length);
     display.innerHTML = cards[card];
     card = Math.round(Math.random() * cards.length);
     display.innerHTML = display.innerHTML + ' ' + cards[card];
 }
+ runGame();
